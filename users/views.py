f6c29f32cd9c9d76c86a4685b308830d071cf71a
Simon Chanzu <chanzusimon6@gmail.com>
2025-02-26 13:03:05 +0300

    change

--- users/views.py
+++ users/views.py
@@ -44,6 +44,8 @@ class LoginView(APIView):
                 "refresh": str(refresh),
                 "user": {
                     "id": user.id,
+                    "first_name":user.first_name,
+                    "last_name":user.last_name,
                     "username": user.username,
                     "email": user.email,
                 }
