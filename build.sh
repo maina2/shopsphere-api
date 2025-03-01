a3dfe5e9004ff6e252b070c0759a81fba0499c30
Simon Chanzu <chanzusimon6@gmail.com>
2025-03-01 16:49:47 +0300

    commits

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
-
-
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+# First fake the admin migrations since that's what's causing the error
+python manage.py migrate admin --fake
+
+# Then run the rest of the migrations with fake-initial
+python manage.py migrate --fake-initial
+
+# Run regular migrate to catch anything that actually needs to be migrated
 python manage.py migrate
